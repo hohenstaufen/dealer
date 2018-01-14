78bdd8d4724c273132d9170754099716989ebdda
Alberto Buratti <alberto.buratti@gtsu.org>
2018-01-14 08:35:51 +0000

    fixes for django 2.0

--- dealer/contrib/django/staff.py
+++ dealer/contrib/django/staff.py
@@ -12,12 +12,20 @@ def context_processor(request):
     return dict(REVISION=BACKEND.revision, TAG=BACKEND.tag)
 
 
-class Middleware(object):
+class Middleware:
 
     """ Append current SCM revision to request object. """
 
-    @staticmethod
-    def process_request(request):
-        """ Add revision and tag to request. """
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
         request.revision = BACKEND.revision
         request.tag = BACKEND.tag
+        response = self.get_response(request)
+        return response
+    
+#    def process_request(self, request):
+#        """ Add revision and tag to request. """
+#        request.revision = BACKEND.revision
+#        request.tag = BACKEND.tag
